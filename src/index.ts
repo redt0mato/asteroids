0aa2046eaf79952134bb088355174cfa3534d56f
Yi Chao <changingtimes@gmail.com>
2022-07-27 19:15:26 -0700

    Moved files to src file and updated tsc compiler

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ const canvas = document.getElementsByTagName('canvas')[0];
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
