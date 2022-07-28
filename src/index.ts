d3872fd3d9ab95b61a73d1961de13d1f7bc0a568
Yi Chao <changingtimes@gmail.com>
2022-07-27 19:35:51 -0700

    used typedefs to make explicit connection between ctx.rect and Entity draw method

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,27 @@ const canvas = document.getElementsByTagName('canvas')[0];
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
+
+/*
+  This is how we draw the entity and to use types to make explicit the relationship
+*/
+type EntityDrawingArgumentsParams = Parameters<typeof ctx.rect>
+
+/*
+  Common class for how we draw an entity in Asteroids
+*/
+class Entity {
+  
+  x: EntityDrawingArgumentsParams[0];
+  y: EntityDrawingArgumentsParams[1];
+  width: EntityDrawingArgumentsParams[2];
+  height: EntityDrawingArgumentsParams[3];
+
+  draw(context: typeof ctx, ) {
+
+  }
+}
+ 
