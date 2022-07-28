310eda6862f3fa3cf7a6d163be0bd0d6456acec2
Yi Chao <changingtimes@gmail.com>
2022-07-28 11:24:25 -0700

    Added JSDoc information for entity

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,22 @@ ctx.closePath();
 */
 type EntityDrawingArgumentsParams = Parameters<typeof ctx.rect>
 
-/*
-  Common class for how we draw an entity in Asteroids
-*/
+
+/**
+ * Common class to store drawing and positioning an entity in Asteroids. 
+ *
+ * @remarks
+ * Eg asteroids, bullets, and the ship. Will contain power-up. 
+ *
+ * @param x - x position on a 2D grid.
+ * @param y - y position on a 2D grid.
+ * @param w - width of a rectangle.
+ * @param h - height of a rectangle
+ * @returns An instance we can use to draw shapes
+ *
+ */
 class Entity {
-  
+
   x: EntityDrawingArgumentsParams[0];
   y: EntityDrawingArgumentsParams[1];
   width: EntityDrawingArgumentsParams[2];
@@ -26,4 +37,6 @@ class Entity {
 
   }
 }
+
+const newEntityInstance = new Entity();
  
