5aa63667ad606451f22910550efe7400dbc7f78d
Yi Chao <changingtimes@gmail.com>
2022-07-27 19:09:49 -0700

    Getting the color filled out

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-console.log("hello world");
-console.log("it worked!")
+const canvas = document.getElementsByTagName('canvas')[0];
+
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
